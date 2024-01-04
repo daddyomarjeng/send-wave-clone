3e681c3848baf300188755806e4e4b44b66ec3d7
DOJ <omarjeng999@gmail.com>
2024-01-04 15:35:45 +0000

    add number pad functionality

--- components/PhoneInput.jsx
+++ components/PhoneInput.jsx
@@ -20,6 +20,23 @@ const PhoneInput = () => {
   const [phone, setPhone] = useState("");
   const [showBlinker, setShowBlinker] = useState(true);
 
+  const handleChange = (input) => {
+    let val;
+    if (input === "-1") {
+      if (phone?.length <= 0 || !phone) {
+        val = "";
+      } else {
+        val = phone.toString().substring(0, phone.length - 1);
+      }
+    } else if ((!phone && input === "0") || (!phone && input === "00")) {
+      val = "";
+    } else {
+      val = phone ? phone + input : input;
+    }
+
+    setPhone(val);
+  };
+
   useEffect(() => {
     // Change the state every second or the time given by User.
     const interval = setInterval(() => {
@@ -51,24 +68,27 @@ const PhoneInput = () => {
       </View>
       <View style={styles.bottom}>
         <View style={styles.row}>
-          <NumberPad number={1} />
-          <NumberPad number={2} />
-          <NumberPad number={3} />
+          <NumberPad number={1} onPress={() => handleChange("1")} />
+          <NumberPad number={2} onPress={() => handleChange("2")} />
+          <NumberPad number={3} onPress={() => handleChange("3")} />
         </View>
         <View style={styles.row}>
-          <NumberPad number={4} />
-          <NumberPad number={5} />
-          <NumberPad number={6} />
+          <NumberPad number={4} onPress={() => handleChange("4")} />
+          <NumberPad number={5} onPress={() => handleChange("5")} />
+          <NumberPad number={6} onPress={() => handleChange("6")} />
         </View>
         <View style={styles.row}>
-          <NumberPad number={7} />
-          <NumberPad number={8} />
-          <NumberPad number={9} />
+          <NumberPad number={7} onPress={() => handleChange("7")} />
+          <NumberPad number={8} onPress={() => handleChange("8")} />
+          <NumberPad number={9} onPress={() => handleChange("9")} />
         </View>
         <View style={styles.row}>
-          <NumberPad number={""} />
-          <NumberPad number={0} />
-          <NumberPad number={""} />
+          <NumberPad number={"\t\t\t\t\t"} disabled />
+          <NumberPad number={0} onPress={() => handleChange("0")} />
+          <NumberPad
+            number={<Ionicons name="backspace" size={30} />}
+            onPress={() => handleChange("-1")}
+          />
         </View>
       </View>
     </View>
@@ -118,6 +138,7 @@ const styles = StyleSheet.create({
   bottom: {
     flex: 1,
     justifyContent: "flex-end",
+    paddingHorizontal: 10,
   },
   row: {
     flexDirection: "row",
@@ -125,10 +146,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   numPad: {
-    padding: 30,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
   },
   numPadText: {
-    fontSize: 28,
+    fontSize: 32,
     color: "#777",
     fontWeight: "bold",
   },
