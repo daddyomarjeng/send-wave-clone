f1ca515ffc87431e283a3534be76ddb7dcbb6ed9
DOJ <omarjeng999@gmail.com>
2024-01-19 16:46:52 +0000

    transaction item design done

--- screens/home/TransactionItem.jsx
+++ screens/home/TransactionItem.jsx
@@ -1,14 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { COLORS } from "../../constant/colors";
 
 const TransactionItem = () => {
   return (
-    <View>
-      <Text>TransactionItem</Text>
+    <View style={styles.container}>
+      <View style={styles.left}>
+        <Text style={styles.text} numberOfLines={2}>
+          Withdrawal
+        </Text>
+        <Text style={styles.textLight}>Jul 04, 2023</Text>
+      </View>
+      <View style={styles.right}>
+        <Text style={styles.text}>1,000</Text>
+      </View>
     </View>
   );
 };
 
 export default TransactionItem;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  left: {},
+  right: {},
+  text: { fontSize: 20, color: COLORS.bg, fontWeight: "bold" },
+  textLight: {
+    fontSize: 16,
+    color: "#999",
+  },
+});
