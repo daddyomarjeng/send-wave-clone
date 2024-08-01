5c35b97e26252c69b382da86fef6d0a8de300918
DOJ - Daddy Omar Jeng <omarjeng999@gmail.com>
2024-08-01 23:32:14 +0000

    fix send to new number

--- screens/send/NewNumber.jsx
+++ screens/send/NewNumber.jsx
@@ -3,12 +3,35 @@ import React from "react";
 import SendMoneyLayout from "../../layouts/SendMoneyLayout";
 import AppTextInput from "../../components/AppTextInput";
 import PhoneInput from "../../components/PhoneInput";
+import AppCountryPicker from "../../components/AppCountryPicker";
+import { COLORS } from "../../constant/colors";
 
 const NewNumberScreen = () => {
   return (
     <SendMoneyLayout>
       <AppTextInput label={"Full Name"} autoFocus style={styles.fullName} />
-      <PhoneInput />
+      {/* <PhoneInput /> */}
+      <View style={styles.row}>
+        <AppCountryPicker
+          // onSelect={(country) => console.log(country)}
+          onSelect={(country) => {
+            // setCallingCode(country?.callingCode)
+          }}
+          callingCodeSize={1}
+          flagSize={16}
+          br={5}
+          py={14}
+          initialCountry={"+220"}
+        />
+        <AppTextInput
+          style={styles.input}
+          placeholder="354XXXX"
+          onChangeText={(text) => {
+            //
+          }}
+          keyboardType="numeric"
+        />
+      </View>
     </SendMoneyLayout>
   );
 };
@@ -18,5 +41,15 @@ export default NewNumberScreen;
 const styles = StyleSheet.create({
   fullName: {
     marginTop: 30,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    marginLeft: 8,
+    marginBottom: 8,
   },
 });
